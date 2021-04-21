1c4eb87a44ae45aa676724f99c7e30f09be6ebd2
Nathan <nathan.istace@gmail.com>
2021-04-21 21:20:41 +0200

    Moving PunUtils to a distinct repository

---
+++ Events/PunPlayerEvent.cs
@@ -0,0 +1,6 @@
+﻿using Photon.Realtime;
+using UnityEngine.Events;
+
+namespace PunUtils.Events {
+	public class PunPlayerEvent : UnityEvent<Player> { }
+}
---
+++ Events/PunRoomInfoEvent.cs
@@ -0,0 +1,6 @@
+﻿using Photon.Realtime;
+using UnityEngine.Events;
+
+namespace PunUtils.Events {
+	public class PunRoomInfoEvent : UnityEvent<RoomInfo> { }
+}
