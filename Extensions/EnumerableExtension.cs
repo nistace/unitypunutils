7a585d0787aec99e3e86967991500ecdb64c2654
Nathan <nathan.istace@gmail.com>
2021-05-03 20:15:56 +0200

    Fixed Pun utils with the utils updates

--- Extensions/EnumerableExtension.cs
+++ Extensions/EnumerableExtension.cs
@@ -6,7 +6,7 @@ using Utils.Pun.Random;
 namespace Utils.Pun.Extensions {
 	public static class EnumerableExtension {
 		public static E NetworkRandom<E>(this IEnumerable<E> array) => array.Random(PunRandom.Range);
-		public static E NetworkRandom<E>(this IList<E> array, Func<E, float> probability) => array.Random(1, probability, PunRandom.Range).GetSafe(0);
-		public static E[] NetworkRandom<E>(this IList<E> array, int size, Func<E, float> probability) => array.Random(size, probability, PunRandom.Range);
+		public static E NetworkRandom<E>(this IReadOnlyList<E> array, Func<E, float> probability) => array.Random(1, probability, PunRandom.Range).GetSafe(0);
+		public static E[] NetworkRandom<E>(this IReadOnlyList<E> array, int size, Func<E, float> probability) => array.Random(size, probability, PunRandom.Range);
 	}
 }
